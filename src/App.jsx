253fced83aa87dec2698d5453db2f8a3ce612676
Rodrigo Andrade <73497926+Buskik@users.noreply.github.com>
2022-06-15 05:09:06 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Image } from './Image';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 class="duda">TE AMO DUDA</h1>
+      <Image />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Image.jsx
@@ -0,0 +1,7 @@
+export function Image() {
+  return (
+    <div>
+      <img src="https://cdn-icons-png.flaticon.com/512/1076/1076984.png" />
+    </div>
+  );
+}
